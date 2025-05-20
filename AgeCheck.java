8d12645e277871e2b8fa2a62f24fa6965ba5eaf7
nithya187 <mynithyashree@gmail.com>
2025-05-20 13:00:49 +0800

    custom exception class that check age is above 18

--- AgeCheck.java
+++ AgeCheck.java
@@ -4,6 +4,7 @@
 // Create a custom exception class InvalidAgeException that is thrown when the age is less than 18.  
 // note:- try to implememt logic of inheritance(Exception given by java will be parent class the create your custom class) with custom method for this logic
 
+
 // Expected Behavior:
 // Throw the exception if age is below 18 and catch it to print "Age must be 18 or older."
 // Step 1: Create a custom exception class
